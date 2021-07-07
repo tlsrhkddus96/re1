0287b1ff27d842bc31eba00162346a361a023242
tlsrhkddus96 <tlsrhkddus96@users.noreply.github.com>
2021-07-07 17:21:48 +0900

    Re1

---
+++ src/main/java/org/zerock/re1/Re1Application.java
@@ -0,0 +1,13 @@
+package org.zerock.re1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Re1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Re1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/zerock/re1/Re1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.re1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Re1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
